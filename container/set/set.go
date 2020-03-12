d50bb0169ef7f574bbf5b6df8aef76b40cec1543
alimy <alimy@gility.net>
2020-03-13 00:33:26 +0800

    add OnceSet struct

---
+++ container/set/set.go
@@ -0,0 +1,40 @@
+package set
+
+// OnceFunc used after OnceSet add it
+type OnceFunc func(string) error
+
+// OnceSet once set
+type OnceSet interface {
+	Add(string) error
+	Exist(string) bool
+}
+type onceSet struct {
+	inSet    map[string]struct{}
+	onceFunc OnceFunc
+}
+
+// Add add an item to set. Will return an error if onceFunc failure.
+func (s *onceSet) Add(it string) error {
+	if _, exist := s.inSet[it]; !exist {
+		err := s.onceFunc(it)
+		if err != nil {
+			return err
+		}
+		s.inSet[it] = struct{}{}
+	}
+	return nil
+}
+
+// Exist whether it exist
+func (s *onceSet) Exist(it string) bool {
+	_, exist := s.inSet[it]
+	return exist
+}
+
+// New return an OnceSet instance
+func NewOnceSet(onceFunc OnceFunc) OnceSet {
+	return &onceSet{
+		inSet:    make(map[string]struct{}),
+		onceFunc: onceFunc,
+	}
+}
